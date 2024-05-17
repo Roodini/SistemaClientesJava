d8cabe4e102b5abccb89b33d49dc8a6b8944fc3a
roodini <roodinijoaquin@gmail.com>
2024-05-16 22:14:42 -0700

    asd

---
+++ src/main/java/com/mycompany/sistemagestion/SistemaGestion.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.sistemagestion;
+
+import com.mycompany.sistemagestion.forms.Formulario;
+
+/**
+ *
+ * @author roodi
+ */
+public class SistemaGestion {
+
+    public static void main(String[] args) {
+        Formulario ventana = new Formulario();
+        ventana.show();
+    }
+}
